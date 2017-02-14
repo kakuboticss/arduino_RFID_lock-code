92438b801c628918e72d8e6ada125297a7c0d608
Yash Khandelwal <yashkhandelwal27@gmail.com>
2017-02-14 22:25:33 +0530

    Add files via upload

---
+++ rfid_lock.ino
@@ -0,0 +1,83 @@
+/*
+ * ----------------------------------------------------------------------------
+ * This sketch uses the MFRC522 library ; see https://github.com/miguelbalboa/rfid
+ * for further details and other examples.
+ * 
+ * NOTE: The library file MFRC522.h has a lot of useful info. Please read it.
+ * 
+ * This sketch show a simple locking mechanism using the RC522 RFID module.
+ * ----------------------------------------------------------------------------
+ * Typical pin layout used:
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno           Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D10        10               10
+ * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+ *
+ */
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN         9           // Configurable, see typical pin layout above
+#define SS_PIN          10          // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+
+String read_rfid;
+String ok_rfid_1="7e6f965";
+int lock=7; 
+
+
+void setup() {
+    Serial.begin(9600);         // Initialize serial communications with the PC
+    while (!Serial);            // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+    SPI.begin();                // Init SPI bus
+    mfrc522.PCD_Init();         // Init MFRC522 card
+
+   
+    pinMode(lock, OUTPUT);
+    
+}
+
+/*
+ * Helper routine to dump a byte array as hex values to Serial.
+ */
+void dump_byte_array(byte *buffer, byte bufferSize) {
+    read_rfid="";
+    for (byte i = 0; i < bufferSize; i++) {
+        read_rfid=read_rfid + String(buffer[i], HEX);
+    }
+}
+
+void open_lock() {
+
+  digitalWrite(lock, HIGH);
+  delay(2000);
+  digitalWrite(lock,LOW);
+  
+ 
+}
+
+void loop() {
+
+      // Look for new cards
+    if ( ! mfrc522.PICC_IsNewCardPresent())
+        return;
+
+    // Select one of the cards
+    if ( ! mfrc522.PICC_ReadCardSerial())
+        return;
+
+    dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
+    Serial.println(read_rfid);
+    if (read_rfid==ok_rfid_1) {
+      
+      open_lock();
+    }
+    
+}
